635d072559b04e69a1a00dc11b018a16ab698a80
Leon Rosengarten <leonr@brioanimation.com>
2018-12-17 16:18:37 +0200

    added device project

---
+++ transponder_device/transponder_device.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		transponder_device.ino
+ Created:	12/17/2018 4:17:36 PM
+ Author:	leonr
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
